f07ef8864e318e627f7cfbd26657d2ed4dfb4c2c
Anita Wangui <anitawangui22@gmail.com>
2024-11-11 12:08:40 +0300

    add search and add plant features

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,23 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import PlantCard from "./PlantCard";
 
-function PlantList() {
-  const [plants, setPlants] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:6001/plants")
-      .then((response) => response.json())
-      .then((data) => setPlants(data))
-      .catch((error) => console.error("Error fetching plants:", error));
-  }, []);
-
-  return (
-    <ul className="cards">
-      {plants.map((plant) => (
-        <PlantCard key={plant.id} plant={plant}/>
-      ))}
-    </ul>
-  );
-}
+const PlantList = ({ plants }) => (
+  <ul className="cards">
+    {plants.map((plant) => (
+      <PlantCard key={plant.id} plant={plant} />
+    ))}
+  </ul>
+);
 
 export default PlantList;
